0f9b7368d8343c2105a1defbe25c059277ede68a
Bijay Regmi <wilson_blackman@yahoo.com>
2020-11-28 12:49:18 +0100

    Updated Long Description in PyPI package

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ with open("README.md","r") as f:
     long_description = f.read()
 setuptools.setup(
     name='strato-dyndns',
-    version='1.0.0',
+    version='1.0.1',
     scripts = ['strato-dyndns/strato-dyndns'],
     packages=setuptools.find_packages() ,
     url='https://github.com/regmibijay/strato-dyndns',
     license='MIT',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     author='Bijay Regmi',
     author_email='strato-dyndns@regdelivery.de',
     description='Updates your DNS records on Strato DNS.',
@@ -16,7 +18,7 @@ setuptools.setup(
     python_requires = '>=3.5',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ]
 )
