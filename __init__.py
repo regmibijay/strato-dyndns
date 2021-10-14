8dc12150d6579401714c66f3650ebc30c89e0399
Bijay Regmi <wilson_blackman@yahoo.com>
2021-10-14 17:55:56 +0200

    version 1.1, adopts black standard

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .strato-dyndns import *
-name = "strato-dyndns"
-__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import setuptools
 
-with open("README.md","r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
+
 setuptools.setup(
-    name='strato-dyndns',
-    version='1.0.4',
-    scripts = ['strato-dyndns/strato-dyndns'],
-    packages=setuptools.find_packages() ,
-    url='https://github.com/regmibijay/strato-dyndns',
-    license='GNU (GPLv3)',
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    author='Bijay Regmi',
-    author_email='strato-dyndns@regdelivery.de',
-    description='Updates your DNS records on Strato DNS.',
-    install_requires = [''],
-    python_requires = '>=3.5',
+    name="strato-dyndns",
+    version="1.1.0",
+    scripts=["strato-dyndns/strato-dyndns"],
+    packages=setuptools.find_packages(),
+    url="https://github.com/regmibijay/strato-dyndns",
+    license="GNU (GPLv3)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Bijay Regmi",
+    author_email="strato-dyndns@regdelivery.de",
+    description="Updates your DNS records on Strato DNS.",
+    install_requires=[""],
+    python_requires=">=3.5",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
